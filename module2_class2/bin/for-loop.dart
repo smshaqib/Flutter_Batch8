5d641153375071b966541146d64b715656ccadbf
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2024-09-04 00:11:03 +0600

    added

---
+++ module2_class2/bin/for-loop.dart
@@ -0,0 +1,23 @@
+main(){
+
+  for(int i=0; i<100; i++){
+
+    if(i==20) {
+      continue;
+    }
+    print(i);
+
+    if(i==25){
+      break;
+    }
+  }
+
+
+  int n=0;
+
+  while(n<=10){
+    print(n);
+    n++;
+  }
+
+}
---
+++ module2_class2/bin/if-else.dart
@@ -0,0 +1,20 @@
+main(){
+
+  double weather = 23.50;
+
+  if(weather<25){
+
+    print("Rest");
+  }
+  else if(weather>=25 && weather<=30){
+
+    print('go to gym');
+  }
+  else if(weather<100){
+    print("hell");
+  }
+  else{
+    print("will do work");
+  }
+
+}
