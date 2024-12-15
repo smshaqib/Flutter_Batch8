e17a461dd1e7e6e842f8e0c04c01824cee39d09d
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2024-12-15 16:15:05 +0600

    added

---
+++ module12_class1/lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:module12_class1/screens/todo_list_screen.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TodoListScreen(),
+    );
+  }
+}
---
+++ module12_class1/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:module12_class1/app.dart';
+
+
+void main() {
+  runApp(const TodoApp());
+}
